91dcc37f0a46cfcb721ab990d7fcd8299926f331
Nima <nimaaap85@gmail.com>
2019-04-18 18:12:51 +0430

    traccar test

---
+++ WebApplicationPostgresTest/Controllers/TCController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationPostgresTest.ViewModel;
+
+namespace WebApplicationPostgresTest.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/TC")]
+    public class TCController : BaseController
+    {
+        public TCController()
+        {
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> Add([FromBody] TCModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return SendResponse("BadRequset");
+            }
+            //var response = await positionLogic.Add(model);
+            return SendResponse("GoodRequset");
+        }
+    }
+}
---
+++ WebApplicationPostgresTest/ViewModel/TCModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationPostgresTest.ViewModel
+{
+    public class TCModel
+    {
+        public string id { get; set; }
+        public DateTime timestamp { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public double speed { get; set; }
+        public short bearing { get; set; }
+        public double altitude { get; set; }
+        public double accuracy { get; set; }
+        public double batt { get; set; }
+    }
+}
